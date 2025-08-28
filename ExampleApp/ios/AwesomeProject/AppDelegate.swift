91c647b2aa8c84ed4c1e040151bc6456fbe76e7a
zhanim-tdl <zhani.muceku@testdevlab.com>
2025-08-28 17:13:54 +0200

    feat: updating sdk, rename app, fix token and ui

--- ExampleApp/ios/AwesomeProject/AppDelegate.swift
+++ ExampleApp/ios/AwesomeProject/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "AwesomeProject",
+      withModuleName: "ExampleApp",
       in: window,
       launchOptions: launchOptions
     )
---
+++ ExampleApp/src/access-token.ts
@@ -0,0 +1,1 @@
+export const token = 'YOUR_ACCESS_TOKEN';
